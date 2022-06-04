525416562d6e3a83dd1a194254af092f2ddad508
hohnuki <ohnukihiroki8585@yahoo.co.jp>
2022-06-04 23:11:51 +0900

    main.cのスペースをタブに変更した

--- main.c
+++ main.c
@@ -6,35 +6,35 @@
 
 void exec(char *str)
 {
-    char **cmd = ft_split(str, ' ');
-    if (ft_strncmp(cmd[0], "/bin/", 5) != 0)
-        cmd[0] = ft_strjoin("/bin/", cmd[0]);
-    int pid = fork();
-    if (pid == 0)
-    {
-        execve(cmd[0], cmd, NULL);
-        exit (127);
-    }
-    
-    int status;
-    wait(&status);
-    if (WEXITSTATUS(status) == 127)
-        printf("minishell: %s: command not found.\n", cmd[0]);
+	char **cmd = ft_split(str, ' ');
+	if (ft_strncmp(cmd[0], "/bin/", 5) != 0)
+		cmd[0] = ft_strjoin("/bin/", cmd[0]);
+	int pid = fork();
+	if (pid == 0)
+	{
+		execve(cmd[0], cmd, NULL);
+		exit (127);
+	}
+
+	int status;
+	wait(&status);
+	if (WEXITSTATUS(status) == 127)
+		printf("minishell: %s: command not found.\n", cmd[0]);
 }
 
 
 int main(int argc, char **argv, char **envp)
 {
-    (void)argc;
-    (void)argv;
-    while (1)
-    {
-        char *str = readline("nukishell: ");
-        add_history(str);// "ls | cat" -> {"ls", "|", "cat"}(use split)
-        t_path *path = create_path(envp);
-        exec(str);
-    }
-    return 0;
+	(void)argc;
+	(void)argv;
+	while (1)
+	{
+		char *str = readline("nukishell: ");
+		add_history(str);// "ls | cat" -> {"ls", "|", "cat"}(use split)
+		t_path *path = create_path(envp);
+		exec(str);
+	}
+	return 0;
 }
 
 //
