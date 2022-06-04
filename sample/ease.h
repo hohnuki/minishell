d79a142dc9ca558c744e067f282c86f218b82b5e
小平波琉 <h.kopei.89315167@icloud.com>
2022-06-04 18:44:47 +0900

    make test追加 && file分割

---
+++ sample/ease.h
@@ -0,0 +1,13 @@
+#ifndef EASE_H
+#define EASE_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *ft_strjoin(char *s1, char *s2);
+
+#endif
