0bd67b93a5b4e340fc38b291dd42e2fbcfc3bf85
hohnuki <ohnukihiroki8585@yahoo.co.jp>
2022-06-04 22:11:07 +0900

    main.c > execのインデントを詰めた

--- main.c
+++ main.c
@@ -7,20 +7,19 @@
 void exec(char *str)
 {
 	char **cmd = ft_split(str, ' ');
-		if (ft_strncmp(cmd[0], "/bin/", 5) != 0)
-			cmd[0] = ft_strjoin("/bin/", cmd[0]);
-		int pid = fork();
-		if (pid == 0)
-		{
-			execve(cmd[0], cmd, NULL);
-			exit (127);
-		}
-			
-		int status;
-		wait(&status);
-		if (WEXITSTATUS(status) == 127)
-			printf("minishell: %s: command not found.\n", cmd[0]);
+	if (ft_strncmp(cmd[0], "/bin/", 5) != 0)
+		cmd[0] = ft_strjoin("/bin/", cmd[0]);
+	int pid = fork();
+	if (pid == 0)
+	{
+		execve(cmd[0], cmd, NULL);
+		exit (127);
+	}
 
+	int status;
+	wait(&status);
+	if (WEXITSTATUS(status) == 127)
+		printf("minishell: %s: command not found.\n", cmd[0]);
 }
 
 
