ef787519145ea3ad9987082ba17e81bc5144cdd4
小平波琉 <h.kopei.89315167@icloud.com>
2022-06-04 21:15:30 +0900

    #2 input OK

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include "minishell.h"
 
-char *cmd[] = {"/bin/ls", NULL};
+//char *cmd[] = {"/bin/ls", NULL};
+//execve(cmd[0], cmd, NULL);
 
 int main(int argc, char **argv, char **envp)
 {
-	execve(cmd[0], cmd, NULL);
+	while (1)
+	{
+		char *str = readline("nukishell: ");
+		add_history(str);// /bin/ls
+		
+		char *cmd[] = {str, NULL};
+		execve(cmd[0], cmd, NULL);
+	}
 	return 0;
 }
